9646fcfe32f5bf62167f0f8215f47808989eb9a0
mcsof <87890325+mcsof@users.noreply.github.com>
2026-05-13 18:25:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 from io import BytesIO
 
 # =====================================================
